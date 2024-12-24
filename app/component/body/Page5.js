f7c13d5168efe3364f560362e6d7be262be50216
imam hossen <imam.frontend404@gmail.com>
2024-12-24 13:32:27 +0600

    displaing

--- app/component/body/Page5.js
+++ app/component/body/Page5.js
@@ -11,7 +11,7 @@ const Page5 = () => {
                     <div className='flex-1 relative '>
                         <div className='flex flex-col'>
                             <h2 className=" sebstiion text-[25px] sm:text-[30px] md:text-[35px] lg:text-[42px] xl:text-[53px] 2xl:text-[62px] text-[#05181F]">What we’re thinking about.</h2>
-                            <p className='kalarna text-[13px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px] pt-[24px] lg:pt-[32px] !text-[#05181fcc]'>For our take on investment themes, industries and the <br/> latest news on the gamechangers we've partnered with.</p>
+                            <p className='kalarna text-[13px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px] pt-[24px] lg:pt-[32px] !text-[#05181fcc]'>For our take on investment themes, industries and the <br/> latest news os the gamecdangers we've partnered with.</p>
                             <button className='flex justify-start items-srart gap-[8px] sm:gap-[12px] readmore text-[15px] sm:text-[16px] md:text-[18px] lg:text-[20px] xl:text-[22px] 2xl:text-[24px] mt-[20px] md:mt-[30px] xl:mt-[40px]'>Why Atomico is different<Image className="" src={image12} alt='' /></button>
                         </div>
                         <div className='flex items-center gap-2 sm:gap-4 smmt-[0] mt-[20px] sm:absolute bottom-0'>
